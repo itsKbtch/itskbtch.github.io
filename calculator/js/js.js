aa7487f20ffa7b6d0195ea0c2fe7ecb3bc19b953
itsKbtch <itsKbtch@users.noreply.github.com>
2018-07-28 12:35:27 +0700

    ver4.0

--- calculator/js/js.js
+++ calculator/js/js.js
@@ -1,96 +1,96 @@
 			function input(a) {
 				if (ans == true) {
-					document.getElementById("screen2").value = "";
-					document.getElementById("screen2").value = document.getElementById("screen2").value + a.innerText;
+					$("#screen2")[0].value = "";
+					$("#screen2")[0].value = $("#screen2")[0].value + a.innerText;
 					ans = false;
 					} else {
-						document.getElementById("screen2").value = document.getElementById("screen2").value + a.innerText;
+						$("#screen2")[0].value = $("#screen2")[0].value + a.innerText;
 						}
 			}
 			function inputC(a) {
-				if(document.getElementById("screen2").value != "" && document.getElementById("screen2").value.includes(".") == false){
+				if($("#screen2")[0].value != "" && $("#screen2")[0].value.includes(".") == false){
 					input(a);
 				}
 			}
 			function inputPi() {
-				document.getElementById("screen2").value = Math.PI;
+				$("#screen2")[0].value = Math.PI;
 				ans=true;
 			}
 			function del() {
-				document.getElementById("screen2").value = "";
+				$("#screen2")[0].value = "";
 				ans = false;
 			}
 			function delA() {
-				document.getElementById("screen1").value = "";
-				document.getElementById("screen2").value = "";
+				$("#screen1")[0].value = "";
+				$("#screen2")[0].value = "";
 				ans = false;
 				powCheck= false;
 			}
 			function cal(a) {
 				if (powCheck != true) {
-				if(document.getElementById("screen2").value != ""){
-					if (document.getElementById("screen2").value.startsWith("-")){
-						document.getElementById("screen1").value =document.getElementById("screen1").value+"("+  document.getElementById("screen2").value+ ") " + a.innerText + " ";
-				document.getElementById("screen2").value="";
+				if($("#screen2")[0].value != ""){
+					if ($("#screen2")[0].value.startsWith("-")){
+						$("#screen1")[0].value =$("#screen1")[0].value+"("+  $("#screen2")[0].value+ ") " + a.innerText + " ";
+				$("#screen2")[0].value="";
 					} else {
-						document.getElementById("screen1").value =document.getElementById("screen1").value+  document.getElementById("screen2").value+ " " + a.innerText + " ";
-						document.getElementById("screen2").value="";
+						$("#screen1")[0].value =$("#screen1")[0].value+  $("#screen2")[0].value+ " " + a.innerText + " ";
+						$("#screen2")[0].value="";
 					}
 				} else {
-					if (document.getElementById("screen1").value != ""){
-					document.getElementById("screen1").value =document.getElementById("screen1").value.substr(0, document.getElementById("screen1").value.length - 2) + a.innerText + " "; }
+					if ($("#screen1")[0].value != ""){
+					$("#screen1")[0].value =$("#screen1")[0].value.substr(0, $("#screen1")[0].value.length - 2) + a.innerText + " "; }
 				}
 				} else {
-					if (document.getElementById("screen2").value != "") {
-						document.getElementById("screen1").value =document.getElementById("screen1").value+ document.getElementById("screen2").value+ ") " + a.innerText + " ";
-						document.getElementById("screen2").value="";
+					if ($("#screen2")[0].value != "") {
+						$("#screen1")[0].value =$("#screen1")[0].value+ $("#screen2")[0].value+ ") " + a.innerText + " ";
+						$("#screen2")[0].value="";
 						powCheck= false;
 					}
 				}
 			}
 			function calMinus(a) {
-				if(document.getElementById("screen2").value != ""){
-					if (document.getElementById("screen2").value.startsWith("-") == false) {
-						document.getElementById("screen2").value = "-" + document.getElementById("screen2").value;
+				if($("#screen2")[0].value != ""){
+					if ($("#screen2")[0].value.startsWith("-") == false) {
+						$("#screen2")[0].value = "-" + $("#screen2")[0].value;
 					} else {
-						document.getElementById("screen2").value = document.getElementById("screen2").value.slice(1);
+						$("#screen2")[0].value = $("#screen2")[0].value.slice(1);
 					}
 				}
 			}
 			var ans;
 			function equal() {
-					if(document.getElementById("screen2").value != ""){
+					if($("#screen2")[0].value != ""){
 						if (powCheck==true){
-							var result = (document.getElementById("screen1").value + document.getElementById("screen2").value + ")").replace(/x|÷/gi, function (x) {switch (x) {case "x": return "*"; break; case "÷": return "/"; break;}});
+							var result = ($("#screen1")[0].value + $("#screen2")[0].value + ")").replace(/x|÷/gi, function (x) {switch (x) {case "x": return "*"; break; case "÷": return "/"; break;}});
 							try {eval(result);} catch (e) {if(typeof e != "number"){alert ("Math Error");}}
-							document.getElementById("screen2").value = eval(result);
-							document.getElementById("screen1").value = "";
+							$("#screen2")[0].value = eval(result);
+							$("#screen1")[0].value = "";
 							ans = true;
 							powCheck = false;
 						} else {
-							var result = (document.getElementById("screen1").value + "(" + document.getElementById("screen2").value + ")").replace(/x|÷/gi, function (x) {switch (x) {case "x": return "*"; break; case "÷": return "/"; break;}});
+							var result = ($("#screen1")[0].value + "(" + $("#screen2")[0].value + ")").replace(/x|÷/gi, function (x) {switch (x) {case "x": return "*"; break; case "÷": return "/"; break;}});
 							try {eval(result);} catch (e) {if(typeof e != "number"){alert ("Math Error");}}
-							document.getElementById("screen2").value = eval(result);
-							document.getElementById("screen1").value = "";
+							$("#screen2")[0].value = eval(result);
+							$("#screen1")[0].value = "";
 							ans = true;}
 					} else {
-						var result = (document.getElementById("screen1").value + 0).replace(/x|÷/gi, function (x) {switch (x) {case "x": return "*"; break; case "÷": return "/"; break;}});
+						var result = ($("#screen1")[0].value + 0).replace(/x|÷/gi, function (x) {switch (x) {case "x": return "*"; break; case "÷": return "/"; break;}});
 						result = result.replace(/\s/gi,"");
 						try {eval(result);} catch (e) {if(typeof e != "number"){alert ("Math Error");}}
-						document.getElementById("screen2").value = eval(result);
-						document.getElementById("screen1").value = "";
+						$("#screen2")[0].value = eval(result);
+						$("#screen1")[0].value = "";
 						ans = true;
 						}
 			} 
 			function fact() {
-				if (document.getElementById("screen2").value != "") {
-				var n=document.getElementById("screen2").value;
+				if ($("#screen2")[0].value != "") {
+				var n=$("#screen2")[0].value;
 				var l;
 				if (Number.isInteger(Number(n))==true && Number(n)>=0) {
 						for (l=1, i=1; i <= Number(n); i++) {
 						l=l*i;
 						}
-						document.getElementById("screen2").value = l;
+						$("#screen2")[0].value = l;
 						ans = true;
 					} else {
 						alert ("Math Error");
@@ -98,9 +98,9 @@
 				}
 			}
 			function pow(a) {
-				if (document.getElementById("screen2").value != "") {
-					if (document.getElementById("screen2").value != "-"){
-						document.getElementById("screen2").value = Math.pow(Number(document.getElementById("screen2").value), a);
+				if ($("#screen2")[0].value != "") {
+					if ($("#screen2")[0].value != "-"){
+						$("#screen2")[0].value = Math.pow(Number($("#screen2")[0].value), a);
 						ans = true;
 					} else {
 						alert ("Math Error");
@@ -109,10 +109,10 @@
 			}
 			var powCheck;
 			function powC() {
-				if(document.getElementById("screen2").value != "" && powCheck != true){
-					if (document.getElementById("screen2").value != "-"){
-						document.getElementById("screen1").value =document.getElementById("screen1").value+ "Math.pow("+ document.getElementById("screen2").value+ ",";
-						document.getElementById("screen2").value="";
+				if($("#screen2")[0].value != "" && powCheck != true){
+					if ($("#screen2")[0].value != "-"){
+						$("#screen1")[0].value =$("#screen1")[0].value+ "Math.pow("+ $("#screen2")[0].value+ ",";
+						$("#screen2")[0].value="";
 						powCheck=true;
 					} else {
 						alert ("Math Error");
@@ -120,15 +120,15 @@
 				}
 			}
 			function exp() {
-				if (document.getElementById("screen2").value != "") {
-						document.getElementById("screen2").value = Math.exp(Number(document.getElementById("screen2").value));
+				if ($("#screen2")[0].value != "") {
+						$("#screen2")[0].value = Math.exp(Number($("#screen2")[0].value));
 						ans=true;
 					} 
 				}
 			function log() {
-				if (document.getElementById("screen2").value != "") {
-					if (Number(document.getElementById("screen2").value) > 0) {
-						document.getElementById("screen2").value = Math.log(Number(document.getElementById("screen2").value));
+				if ($("#screen2")[0].value != "") {
+					if (Number($("#screen2")[0].value) > 0) {
+						$("#screen2")[0].value = Math.log(Number($("#screen2")[0].value));
 						ans=true;
 					} else {
 						alert("Math Error");
@@ -136,35 +136,44 @@
 				}
 			}
 			function sqrt() {
-				if (document.getElementById("screen2").value != "") {
-				if (Number.isInteger(Number(document.getElementById("screen2").value)) && Number(document.getElementById("screen2").value) >= 0) {
-				document.getElementById("screen2").value = Math.sqrt(Number(document.getElementById("screen2").value));
+				if ($("#screen2")[0].value != "") {
+				if (Number.isInteger(Number($("#screen2")[0].value)) && Number($("#screen2")[0].value) >= 0) {
+				$("#screen2")[0].value = Math.sqrt(Number($("#screen2")[0].value));
 				ans=true;
 				} else {
 					alert("Math Error");
 				}}
 			}
 			function percent() {
-				if (document.getElementById("screen2").value != "") {
-						document.getElementById("screen2").value = Number(document.getElementById("screen2").value)/100;
+				if ($("#screen2")[0].value != "") {
+						$("#screen2")[0].value = Number($("#screen2")[0].value)/100;
 						ans=true;
 					}
 			}
 			function sin() {
-				if (document.getElementById("screen2").value != "") {
-						document.getElementById("screen2").value = Math.sin(Number(document.getElementById("screen2").value));
+				if ($("#screen2")[0].value != "") {
+						$("#screen2")[0].value = Math.sin(Number($("#screen2")[0].value));
 						ans=true;
 					}
 			}
 			function cos() {
-				if (document.getElementById("screen2").value != "") {
-						document.getElementById("screen2").value = Math.cos(Number(document.getElementById("screen2").value));
+				if ($("#screen2")[0].value != "") {
+						$("#screen2")[0].value = Math.cos(Number($("#screen2")[0].value));
 						ans=true;
 					}
 			}
 			function tan() {
-				if (document.getElementById("screen2").value != "") {
-						document.getElementById("screen2").value = Math.tan(Number(document.getElementById("screen2").value));
+				if ($("#screen2")[0].value != "") {
+						$("#screen2")[0].value = Math.tan(Number($("#screen2")[0].value));
 						ans=true;
 					}
+			}
+			function change() {
+				var i=Number($('link').attr('href').substr($('link').attr('href').search(".css")-1, 1));
+				if (i<1) {
+					i=i+1;
+				} else {
+					i=0;
+				}
+				$('link').attr('href', "css/style" + i +".css");
 			}
